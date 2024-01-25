b362ab7138a3e9fe36e94f03aa90d7b1793732bf
Aditya Ghidora <adityaghidora@skylarkdrones.com>
2024-01-25 13:28:01 +0530

    added iam urltables

--- handler.py
+++ handler.py
@@ -1,24 +1,48 @@
 import json
+import boto3
+import os
+import datetime
+import random
+import string
+from botocore.exceptions import ClientError
 
+from typing import Any, Dict, Optional
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+URL_TABLE_NAME = os.getenv('URL_TABLE_NAME')
+BASE_URL = os.getenv('BASE_URL')
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+dynamodb = boto3.resource('dynamodb')
+url_table = dynamodb.Table(URL_TABLE_NAME)
 
-    return response
+def get_item(short_id: str) -> Optional[Dict]:
+    try:
+        response = url_table.get_item(
+                Key={
+                    'PK': short_id,
+                    'SK': short_id,
+                }
+        )
+    except ClientError as e:
+        print(f'Error: {e.response}')
+        return {
+                "statusCode": 500,
+                "body": e.response['Error']['Message']
+        }
+    item = response.get('Item', {})
+    return item
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+def get_unique_short_id(size=7, max_iter=10) -> str:
+    short_id = ''
+    curr_iter = 0
+    while (short_id == '' or get_item(short_id) and curr_iter < max_iter):
+        short_id = ''.join(random.choices(string.ascii_lowercase, k=size))
+        curr_iter += 1
+
+    return short_id
+
+def create_short_url(event: Dict[str, Any], context) -> Dict[str, Any]:
+    request_body = json.loads(event['body'])
+    long_url = request_body['longURL']
+    short_id = get_unique_short_id()
+    short_url = f'{BASE_URL}/short/{short_id}'
+
